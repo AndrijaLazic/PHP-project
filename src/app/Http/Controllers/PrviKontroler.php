afc3a1f95a0990ed54f347ce719967ef61f768ec
Gundra <lazic.andrija223@gmail.com>
2025-05-24 00:31:36 +0200

    Xdebug implemented

--- src/app/Http/Controllers/PrviKontroler.php
+++ src/app/Http/Controllers/PrviKontroler.php
@@ -4,12 +4,14 @@ namespace App\Http\Controllers;
 
 use App\Facades\PrvaFacade;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Log;
 
 class PrviKontroler extends Controller
 {
-    
     public function helloWorld()
     {
+        Log::info("Hello World");
+        error_log('Some message here.');
         return PrvaFacade::prvaMetoda();
     }
 
